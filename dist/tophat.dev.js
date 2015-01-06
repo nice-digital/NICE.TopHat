5a51de57c248f7a2e35aa75549ed8a12e8727b68
Matt Smith <mattonfoot@gmail.com>
2015-01-06 16:04:38 +0000

    updated beta links

--- dist/tophat.dev.js
+++ dist/tophat.dev.js
@@ -477,24 +477,22 @@ module.exports = {
     },
     bnf: {
         href: "http://www.evidence.nhs.uk/formulary/bnf/current",
-        beta: "http://beta.evidence.nhs.uk/formulary/bnf/current",
         label: "BNF",
         title: "British National Formulary"
     },
     bnfc: {
         href: "http://www.evidence.nhs.uk/formulary/bnfc/current",
-        beta: "http://beta.evidence.nhs.uk/formulary/bnfc/current",
         label: "BNFC",
         title: "British National Formulary for Children"
     },
     cks: {
         href: "http://cks.nice.org.uk",
-        beta: "http://beta.cks.nice.org.uk",
         label: "CKS",
         title: "Clinical Knowledge Summaries"
     },
     journals: {
         href: "http://www.evidence.nhs.uk/about-evidence-services/journals-and-databases",
+        beta: "http://beta.nice.org.uk/about/what-we-do/evidence-services/journals-and-databases",
         label: "Journals and databases",
         title: "Journals and databases"
     }
