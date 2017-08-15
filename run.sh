7edf7bed05839a60e96573a11ad3635fd9559430
David Wong <wongcheming@gmail.com>
2017-08-15 14:23:15 +0100

    copied from named container

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 docker-compose down && docker-compose rm -vf
 docker-compose build && docker-compose up -d
 docker-compose run tests npm run visualRegression -- --host selenium
-docker cp  nicetophat_tests_1:/tests/screenshots ./screenshots_copy
+docker ps -a
+docker cp $(docker ps -aqf "name=tests_run_1"):/tests/screenshots ./screenshots_copy 
+# docker cp  nicetophat_tests_1:/tests/screenshots ./screenshots_copy
 docker rm -vf $(docker ps -aq) && docker volume rm nicetophat_screenshots
 
 result=$?
