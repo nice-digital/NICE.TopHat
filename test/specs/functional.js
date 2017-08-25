91252a769344d057d23bdff850a81a9f23620bd3
David Wong <wongcheming@gmail.com>
2017-08-25 15:53:49 +0100

    trying teamcity suite organisation

--- test/specs/functional.js
+++ test/specs/functional.js
@@ -1,65 +1,69 @@
 var chai = require("chai");
 chai.should();
 
-describe.only('Given I am on the NICE org site and I click on the Evidence servies button', function(){
-  it('should display the lower menu', async function() {
+describe('Functionally browser driven tests', function() {
+  describe('Given I am on the NICE org site and I click on the Evidence servies button', function(){
+    it('should display the lower menu', async function() {
 
-    browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
+      browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
 
-    browser.click('#menu-evidence');
-    browser.isVisible('.tophat-inner').every((x)=>x).should.be.true;
+      browser.click('#menu-evidence');
+      browser.isVisible('.tophat-inner').every((x)=>x).should.be.true;
+    })
   })
-})
 
-describe.only('Given I am on the NICE org site on a mobile and I click on the Menu button', function(){
-  it('should display the mobile menu', async function() {
+  describe('Given I am on the NICE org site on a mobile and I click on the Menu button', function(){
+    it('should display the mobile menu', async function() {
 
-  	browser.setViewportSize({
-        width: 500,
-        height: 500
-    });
-    browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
+      browser.setViewportSize({
+          width: 500,
+          height: 500
+      });
+      browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
 
-    browser.click('#menu-mobile');
-    browser.isVisible('#main-menu').should.be.true;
+      browser.click('#menu-mobile');
+      browser.isVisible('#main-menu').should.be.true;
+    })
   })
-})
 
-describe.only('Given I am on the NICE org site on a mobile use the keyboard to navigate', function(){
-  it('I can select the main menu using the space bar', async function() {
+  describe('Given I am on the NICE org site on a mobile use the keyboard to navigate', function(){
+    it('I can select the main menu using the space bar', async function() {
 
-  	browser.setViewportSize({
-        width: 500,
-        height: 500
-    });
-    browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
-    browser.keys("Tab");
-    browser.keys(" ");
+      browser.setViewportSize({
+          width: 500,
+          height: 500
+      });
+      browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
+      browser.keys("Tab");
+      browser.keys(" ");
 
-    browser.isVisible('#main-menu').should.be.true;
+      browser.isVisible('#main-menu').should.be.true;
+    })
   })
-})
 
-describe.only('Given I am on the NICE org site on a mobile', function(){
-  it('I can view the profile menu', async function() {
+  describe('Given I am on the NICE org site on a mobile', function(){
+    it('I can view the profile menu', async function() {
 
-  	browser.setViewportSize({
-        width: 500,
-        height: 500
-    });
+      browser.setViewportSize({
+          width: 500,
+          height: 500
+      });
 
-    browser.click("#signin");
+      browser.click("#signin");
 
-    browser.waitForExist("#Email");
+      browser.waitForExist("#Email");
 
-    browser.setValue("#Email", process.env.username);
-    browser.setValue("#Password", process.env.password);
+      browser.setValue("#Email", process.env.username);
+      browser.setValue("#Password", process.env.password);
 
-    browser.submitForm('form');
+      browser.submitForm('form');
 
-    browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
+      browser.url('/example.niceorg.html'); // for base url see wdio.conf.js
 
-	  browser.click('#menu-profile');
-    browser.isVisible('#nice-profile').should.be.true;
+      browser.click('#menu-profile');
+      browser.isVisible('#nice-profile').should.be.true;
+    })
   })
-})
+
+});
+
--- test/unitTests/MenuFunction.js
+++ test/unitTests/MenuFunction.js
@@ -6,69 +6,71 @@ var containerIds = require('../../lib/events/containerIds');
 
 
 
-describe('Given I have controlling elements', function() {
-	it('should give me an array of ids', function() {
+describe('Unit Tests', function() {
+  describe('Given I have controlling elements', function() {
+    it('should give me an array of ids', function() {
 
-		//arrange
-		domUtils.getControllingElementId = sinon.stub().returns("something");
-		domUtils.getIdsOfControlledContainers =  sinon.stub().returns(["randomId"]);
+      //arrange
+      domUtils.getControllingElementId = sinon.stub().returns("something");
+      domUtils.getIdsOfControlledContainers =  sinon.stub().returns(["randomId"]);
 
-		var parentElement = {
-			"id" : "test"
-		};
+      var parentElement = {
+        "id" : "test"
+      };
 
-		//act
-		var result = containerIds(parentElement, domUtils);
+      //act
+      var result = containerIds(parentElement, domUtils);
 
-		//assert
-		expect(result).to.deep.equal(["randomId"]);
-	});
-});
+      //assert
+      expect(result).to.deep.equal(["randomId"]);
+    });
+  });
 
-describe('Given I have no controlling elements', function() {
-	it('should give me id of the parentMenuEl', function() {
+  describe('Given I have no controlling elements', function() {
+    it('should give me id of the parentMenuEl', function() {
 
-		domUtils.getControllingElementId = sinon.stub().returns(null);
-		domUtils.getIdsOfControlledContainers =  sinon.stub().returns(null);
+      domUtils.getControllingElementId = sinon.stub().returns(null);
+      domUtils.getIdsOfControlledContainers =  sinon.stub().returns(null);
 
-		var parentElement = {
-			"id" : "test"
-		};
+      var parentElement = {
+        "id" : "test"
+      };
 
-		var result = containerIds(parentElement, domUtils);
+      var result = containerIds(parentElement, domUtils);
 
-		expect(result).to.deep.equal(["test"]);
-	});
-});
+      expect(result).to.deep.equal(["test"]);
+    });
+  });
 
-describe('Given I have controlling element and no controlled containers', function() {
-	it('should give me null', function() {
+  describe('Given I have controlling element and no controlled containers', function() {
+    it('should give me null', function() {
 
-		domUtils.getControllingElementId = sinon.stub().returns("something");
-		domUtils.getIdsOfControlledContainers =  sinon.stub().returns(null);
+      domUtils.getControllingElementId = sinon.stub().returns("something");
+      domUtils.getIdsOfControlledContainers =  sinon.stub().returns(null);
 
-		var parentElement = {
-			"id" : "test"
-		};
+      var parentElement = {
+        "id" : "test"
+      };
 
-		var result = containerIds(parentElement, domUtils);
+      var result = containerIds(parentElement, domUtils);
 
-		expect(result).to.deep.equal(null);
-	});
-});
+      expect(result).to.deep.equal(null);
+    });
+  });
 
-describe('Given I have a controlled container I have no controlling element', function() {
-	it('should return the parentElementId as array', function() {
+  describe('Given I have a controlled container I have no controlling element', function() {
+    it('should return the parentElementId as array', function() {
 
-		domUtils.getControllingElementId = sinon.stub().returns(null);
-		domUtils.getIdsOfControlledContainers =  sinon.stub().returns("something");
+      domUtils.getControllingElementId = sinon.stub().returns(null);
+      domUtils.getIdsOfControlledContainers =  sinon.stub().returns("something");
 
-		var parentElement = {
-			"id" : "test"
-		};
+      var parentElement = {
+        "id" : "test"
+      };
 
-		var result = containerIds(parentElement, domUtils);
+      var result = containerIds(parentElement, domUtils);
 
-		expect(result).to.deep.equal(["test"]);
-	});
-});
+      expect(result).to.deep.equal(["test"]);
+    });
+  });
+}); 
