0c44ef0ff63db17d102f092a970a12dd28e30a6c
David Wong <wongcheming@gmail.com>
2017-08-25 11:36:56 +0100

    Got the teamcity mocha reporter running

--- tasks/options/mochaTest.js
+++ tasks/options/mochaTest.js
@@ -1,11 +1,14 @@
-module.exports = function() {
+module.exports = function( config ) {
   return {
+    teamcity : {
+      options: {
+        reporter: 'mocha-teamcity-reporter',
+      },
+      src: ['test/unitTests/**/*.js']
+    },
     test : {
       options: {
         reporter: 'spec',
-        quiet: false, // Optionally suppress output to standard out (defaults to false)
-        clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
-        noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
       },
       src: ['test/unitTests/**/*.js']
     }
