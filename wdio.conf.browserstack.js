43049200c19dc744e758adb82f902b0323627ae1
dcmwong <wongcheming@gmail.com>
2017-09-12 16:04:10 +0100

    Update wdio.conf.browserstack.js

--- wdio.conf.browserstack.js
+++ wdio.conf.browserstack.js
@@ -29,12 +29,12 @@ exports.config = {
         name:"Keyboard nav",
         build:"webdriver-tophat"
     },{
-      browserName' : 'IE',
+     'browserName' : 'IE',
      'browser_version' : '8.0',
      'os' : 'Windows',
      'os_version' : '7',
      'resolution' : '1024x768',
-      build:"webdriver-tophat"
+     'build':"webdriver-tophat"
     }],
     sync: true,
     logLevel: 'silent',
