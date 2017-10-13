087aa99a122af513484298a75563b16a45af4e57
ediblecode <ian.routledge@nice.org.uk>
2017-10-13 16:16:48 +0100

    Add extra command parameter for passing in a custom version
    For adding for to the banner at the top of the generated JS.
    Used because TeamCity versions are not valid npm versions so can't be set in package.json

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -5,7 +5,7 @@ var cssify = require('cssify')
 module.exports = {
     options: {
         transform: [ cssify, partialify.onlyAllow(['html']) ],
-        banner: '/*!\n@name <%= pkg.name %>\n@version <%= pkg.version %> | <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n'
+        banner: '/*!\n@name <%= pkg.name %>\n@version <%= grunt.option("buildNumber") || pkg.version %> | <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n'
     },
 
     dist: {
