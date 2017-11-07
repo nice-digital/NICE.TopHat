963f6b78a456ea4097ece43b52512c678571e5ed
ediblecode <ian.routledge@nice.org.uk>
2017-11-07 09:40:38 +0000

    TH-16 :hammer: Refactor browerstack config
    Remove screenshot references as we only run functional tests in browserstack

--- wdio.conf.browserstack.js
+++ wdio.conf.browserstack.js
@@ -1,68 +1,41 @@
 var path = require("path");
 var VisualRegressionCompare = require("wdio-visual-regression-service/compare");
 
-console.log("baseurl", process.env.BASE_URL);
-
-function getScreenshotName(basePath) {
-	return function(context) {
-		var testName = context.test.title;
-		var resolution = context.meta.width || context.meta.orientation || "unknown";
-		var browserVersion = parseInt(/\d+/.exec(context.browser.version)[0]);
-		var browserName = context.browser.name;
-
-		return path.join(basePath, `${testName}_${browserName}_v${browserVersion}_${resolution}.png`);
-	};
-}
+var pkg = require("./package.json");
 
 exports.config = {
-
 	user: process.env.browserStackUser,
 	key: process.env.browserStackKey,
 	specs: [
 		"./test/specs/functional/**/*.js"
 	],
-	// Patterns to exclude.
-	exclude: [
-		// 'path/to/excluded/files'
-	],
 	maxInstances: 10,
-	capabilities: [{
-		browser:"chrome",
-		name:"Keyboard nav",
-		build:"webdriver-tophat",
-		"browserstack.debug" : true
-	},{
-		name:"Keyboard nav",
-		build:"webdriver-tophat",
-		"browserName" : "IE",
-		"browser_version" : "11.0",
-		"os" : "Windows",
-		"os_version" : "10",
-		"resolution" : "1024x768",
-		"browserstack.debug" : true
-	}],
+	capabilities: [
+		// Use https://www.browserstack.com/automate/webdriverio#configure-capabilities
+		{
+			// We use Edge as it's free, but also run the functional tests in headless Chrome in Docker
+			project: "TopHat",
+			name: "Functional tests - Edge",
+			build: "TopHat " + pkg.version,
+			browser: "Edge",
+			os: "Windows",
+			os_version: "10",
+			browser_version: "16.0",
+			resolution: "1024x768",
+			"browserstack.debug": true
+		}
+	],
 	sync: true,
 	logLevel: "silent",
 	coloredLogs: true,
 	bail: 0,
-	screenshotPath: "./errorShots/",
-	//baseUrl: 'http://website:8000',
-	baseUrl: process.env.BASE_URL,
+	baseUrl: process.env.BASE_URL || "http://dev-tophat.nice.org.uk/",
 	waitforTimeout: 10000,
 	connectionRetryTimeout: 90000,
 	connectionRetryCount: 3,
-	plugins: { "wdio-screenshot":{}},
-	services: ["visual-regression", "browserstack"],
-	visualRegression: {
-		compare: new VisualRegressionCompare.LocalCompare({
-			referenceName: getScreenshotName(path.join(process.cwd(), "screenshots/reference")),
-			screenshotName: getScreenshotName(path.join(process.cwd(), "screenshots/taken")),
-			diffName: getScreenshotName(path.join(process.cwd(), "screenshots/diff")),
-			misMatchTolerance: 0.01,
-		})
-	},
+	services: ["browserstack"],
 	framework: "mocha",
-	reporters: ["spec","teamcity"],
+	reporters: ["spec", "teamcity"],
 	mochaOpts: {
 		ui: "bdd",
 		timeout: 600000,
