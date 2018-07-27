38a1fde2062461f19988964cd3579e5df363646c
ediblecode <ian.routledge@nice.org.uk>
2018-07-27 14:23:20 +0100

    TH-30 Update browserstack config

--- wdio.browserstack.conf.js
+++ wdio.browserstack.conf.js
@@ -1,10 +1,12 @@
-var pkg = require("./package.json");
+const config = require("./wdio.conf").config;
 
-exports.config = {
+const pkg = require("./package.json");
+
+const configOverrides = {
 	user: process.env.browserStackUser,
 	key: process.env.browserStackKey,
 	specs: [
-		"./test/specs/functional/**/*.js"
+		"./test/functional/**/*.js"
 	],
 	maxInstances: 10,
 	capabilities: [
@@ -22,22 +24,11 @@ exports.config = {
 			"browserstack.debug": true
 		}
 	],
-	sync: true,
-	logLevel: "silent",
-	coloredLogs: true,
-	bail: 0,
 	baseUrl: process.env.BASE_URL || "http://dev-tophat.nice.org.uk/",
 	waitforTimeout: 10000,
 	connectionRetryTimeout: 90000,
 	connectionRetryCount: 3,
-	services: ["browserstack"],
-	framework: "mocha",
-	reporters: ["spec", "teamcity"],
-	mochaOpts: {
-		ui: "bdd",
-		timeout: 600000,
-		compilers: [
-			"js:babel-register"
-		]
-	}
+	services: ["browserstack"]
 };
+
+exports.config = Object.assign({}, config, configOverrides);
